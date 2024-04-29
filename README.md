26571d186dfe6bb27f027744b1158e967c168577
woozzzy <sunwoo.park0203@gmail.com>
2024-04-29 08:12:27 -0400

    Add basic distillation via spacy NER and summa summarizations

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ CS 4644 Final Project
 ```bash
 conda create -n llama python=3.11 \
 conda activate llama \
-pip install -U torch torchvision torchaudio transformers datasets accelerate huggingface_hub python-dotenv pandas
+pip install torch torchvision torchaudio transformers datasets accelerate huggingface_hub python-dotenv pandas rouge-score spacy summa \
+python -m spacy download en_core_web_sm
 ```
 
 #### HuggingFace Authentication:
--- src/data.py
+++ src/data.py
@@ -1,3 +1,47 @@
-import pandas as pd
-from datasets import Dataset
+import spacy
+from summa import summarizer
+
+from datasets import load_dataset
 from src.logs import logger
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def get_dataset(cfg):
+    dataset = load_dataset(cfg["dataset"]["remote"], cfg["dataset"]["ver"])
+    logger.info(f"Loaded dataset: {cfg['dataset']['remote']}")
+    return dataset
+
+
+def distill(dataset):
+    try:
+        dataset = dataset.map(distill_dataset, batched=True)
+        logger.info("Distilled the dataset")
+        return dataset
+    except Exception as e:
+        logger.error(f"Error in distill: {e}")
+        raise e
+
+
+def distill_article(article):
+    doc = nlp(article)
+    key_entities = set([ent.text for ent in doc.ents if ent.label_ in ["PERSON", "ORG", "GPE"]])
+
+    # Extractive summarization to condense the text
+    distilled_context = summarizer.summarize(article, words=250)  # Extract the most relevant sentences
+
+    # Filter distilled context to retain sentences containing key entities
+    filtered_context = [
+        sentence for sentence in distilled_context.split(".") if any(entity in sentence for entity in key_entities)
+    ]
+
+    return ". ".join(filtered_context)
+
+
+def distill_dataset(dataset):
+    try:
+        dataset["article"] = [distill_article(article) for article in dataset["article"]]
+        return dataset
+    except Exception as e:
+        logger.error(f"Error in distill_dataset: {e}")
+        raise e
