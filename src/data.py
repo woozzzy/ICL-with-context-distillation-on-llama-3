9753aeefd881379be5939b15cbc3e5eb16903575
woozzzy <sunwoo.park0203@gmail.com>
2024-05-01 10:23:33 -0400

    Prompt changes.

--- src/data.py
+++ src/data.py
@@ -19,7 +19,8 @@ def get_dataset(args, train_args, split="train", use_icl=True):
 
         ############################    Base System Prompt    ############################
         
-        prompt = """As a professional summarizer, create a concise and comprehensive summary for ONLY the provided text denoted by \"Document:\". Be sure to include the main ideas and essential information, while eliminating extraneous language and focusing on critical aspects. Rely strictly on the provided text, without including external information. Provided below are example documents and summmarizations. Format your summary to be at most 2 sentences for easy comprehension.\n"""
+        sys_prompt = """In this task, you will help summarize news articles into a single concise sentence. Here are a few examples on how to transform a detailed document into a brief summary:\n"""
+        sys_prompt_msg = {'role': 'system', 'content': sys_prompt}
 
         ############################    In-Context Learning    ############################
         
@@ -39,14 +40,15 @@ def get_dataset(args, train_args, split="train", use_icl=True):
             ## TODO: Implement context distillation
         
             ## Add Context to Prompt
+            instruct_prompt = "" 
             for i, example in enumerate(examples):
-                prompt += f"Example Document {i}: {example['document']}\nExample Summary {i}: {example['summary']}\n\n"
+                instruct_prompt += f"Document: {example['document']}\nSummary: {example['summary']}\n\n"
 
             logger.debug(f"Length of prompt with context: {len(prompt)}")
 
         ############################    Add Prompt to Data    ############################
 
-        prompt += "**Document:**: "
+        prompt += "Now, based on the following document, provide a concise summary."
 
         def _add_prompt(sample):
             sample["document"] = prompt + sample["document"] + "\n" 
@@ -63,15 +65,23 @@ def get_dataset(args, train_args, split="train", use_icl=True):
     return dataset
 
 def get_template(args):
+
     if args.is_instruct:
         return "{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}"
+
     else:
         ## Anthropic/Vicuna like template without the need for special tokens
         return (
-            "{{ sample }}"
-            "{{ sample }}"
+            "{% for message in messages %}"
+            "{% if message['role'] == 'system' %}"
+            "{{ message['content'] }}"
+            "{% elif message['role'] == 'user' %}"
+            "{{ '\n\nHuman: ' + message['content'] +  eos_token }}"
+            "{% elif message['role'] == 'assistant' %}"
+            "{{ '\n\nAssistant: '  + message['content'] +  eos_token  }}"
+            "{% endif %}"
+            "{% endfor %}"
             "{% if add_generation_prompt %}"
-            "{{ '\n\nSummary: ' }}"
+            "{{ '\n\nAssistant: ' }}"
             "{% endif %}"
         )
-
